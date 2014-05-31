2d9271f1a2e51abc3d0e98abec5d1420bb3db60e
Marc DellaVolpe <marc.dellavolpe@gmail.com>
2014-05-30 22:40:21 -0400

    add sync and async interface

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 Sack
 ====
 
-A simple JSON Data Store for Android. 
+A simple JSON Data Store for Android. Under the hood, Sack uses 
+[GSON](https://sites.google.com/site/gson/) and 
+[AtomicFile](https://developer.android.com/reference/android/support/v4/util/AtomicFile.html) to store 
+blobs of JSON.  Sack supports both asynchronous and synchronous usage via AsyncTask and AsyncTask.get();
 
 Example
 =======
@@ -30,11 +33,7 @@ sack.commit(foos, new Sack.Listener<FooList>() {
   }
 });
     
-sack.load(new Sack.Listener<FooList>() {
-  void onResult(Sack.Status status, FooList obj) {
-      
-  }
-}); 
+Pair<Sack.Status, FooList> rv = sack.load().get();    // .get() makes the load synchronous 
 ````
     
     
--- sack/src/main/java/org/quuux/sack/Sack.java
+++ sack/src/main/java/org/quuux/sack/Sack.java
@@ -1,6 +1,8 @@
 package org.quuux.sack;
 
+import android.os.AsyncTask;
 import android.support.v4.util.AtomicFile;
+import android.util.Pair;
 
 import com.google.gson.Gson;
 
@@ -30,19 +32,43 @@ public class Sack<T extends Entity> {
         mPath = path;
     }
 
-    public void load(Listener<T> listener) {
+    private Pair<Status, T> doLoad() {
 
         final AtomicFile file = new AtomicFile(mPath);
 
         try {
-            T obj = mGson.fromJson(new String(file.readFully()), mClass);
-            listener.onResult(Status.SUCCESS, obj);
+            return new Pair<Status, T>(Status.SUCCESS, mGson.fromJson(new String(file.readFully()), mClass));
         } catch (IOException e) {
-            listener.onResult(Status.ERROR, null);
+            return new Pair<Status, T>(Status.ERROR, null);
         }
     }
 
-    public void commit(final T obj, final Listener<T> listener) {
+    public AsyncTask<Void, Void, Pair<Status, T>> load(final Listener<T> listener) {
+
+        final AsyncTask<Void, Void, Pair<Status, T>>  task = new AsyncTask<Void, Void, Pair<Status, T>>() {
+
+            @Override
+            protected Pair<Sack.Status, T> doInBackground(final Void... params) {
+                return doLoad();
+            }
+
+            @Override
+            protected void onPostExecute(final Pair<Sack.Status, T> t) {
+                if (listener != null)
+                    listener.onResult(t.first, t.second);
+            }
+        };
+
+        task.execute();
+
+        return task;
+    }
+
+    public AsyncTask<Void, Void, Pair<Status, T>> load() {
+        return load(null);
+    }
+
+    private Pair<Status, T> doCommit(final T obj) {
         final AtomicFile file = new AtomicFile(mPath);
         FileOutputStream str = null;
         try {
@@ -54,15 +80,41 @@ public class Sack<T extends Entity> {
             str.flush();
             str.close();
             file.finishWrite(str);
-            listener.onResult(Status.SUCCESS, obj);
+            return new Pair<Status, T>(Status.SUCCESS, obj);
         } catch (IOException e) {
             if (str != null)
                 file.failWrite(str);
-            listener.onResult(Status.ERROR, obj);
+            return new Pair<Status, T>(Status.ERROR, null);
         }
+    }
+
+    public AsyncTask<T, Void, Pair<Status, T>> commit(final T obj, final Listener<T> listener) {
+
+        final AsyncTask<T, Void, Pair<Status, T>>  task = new AsyncTask<T, Void, Pair<Status, T>>() {
+
+            @Override
+            protected Pair<Sack.Status, T> doInBackground(final T... params) {
+                return doCommit(params[0]);
+            }
+
+            @Override
+            protected void onPostExecute(final Pair<Sack.Status, T> t) {
+                if (listener != null)
+                    listener.onResult(t.first, t.second);
+            }
+        };
+
+        task.execute();
+
+        return task;
+    }
+
+    public AsyncTask<T, Void, Pair<Status, T>> commit(final T obj) {
+        return commit(obj, null);
     }
 
     public static <T extends Entity> Sack<T> open(final Class<T> entity, final File path) {
         return new Sack<T>(entity, path);
     }
+
 }
